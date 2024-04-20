759a7c775aa96bcf32219961845ede52f23f37f6
Kuzar365 <vothanhphuc365@gmail.com>
2024-04-20 17:59:13 +0700

    Second commit

--- main.c
+++ main.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(void)
+struct product
 {
-    printf("Hello, World!\n");
-    return 0;
+	int id;
+	char product_name[30];
+	int remain;
+	float price;
+};
+
+struct cart
+{
+	int id;
+	char product_id[30];
+	char pruct_name[30];
+	float price;
+	float sum;
+	char coupon[30];
+	float final_sum;
+	int user_id;
+};
+
+struct user
+{
+	int id;
+	char name[30];
+	char address[30];
+	int phone_number;
+};
+
+struct order
+{
+	int id;
+	char product_id[30];
+	char product_name[30];
+	float price;
+	float final_sum;
+	char payment[100];
+	char status[30];
+	int user_id;
+	int cart_id;
+};
+
+int main()
+{
+	FILE *file;
+	file = fopen("product.csv", "r");
 }
