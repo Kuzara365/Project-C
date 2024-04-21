71ef80952e2d7ebadae24827991bbeffb7db3c02
Kuzar365 <vothanhphuc365@gmail.com>
2024-04-21 18:31:21 +0700

    Third commit not finished

--- message.c
+++ message.c
@@ -2,120 +2,59 @@
 #include <string.h>
 #include <stdlib.h>
 
+
 struct product
 {
-	int id;
-	char product_name[30];
-	int remain;
-	float price;
+    int id;
+    char product_name[30];
+    int remain;
+    float price;
 };
 
 struct cart
 {
-	int id;
-	char product_id[30];
-	char pruct_name[30];
-	float price;
-	float sum;
-	char coupon[30];
-	float final_sum;
-	int user_id;
+    int id;
+    char product_id[30];
+    char product_name[30];
+    float price;
+    float sum;
+    char coupon[30];
+    float final_sum;
+    int user_id;
 };
 
 struct user
 {
-	int id;
-	char name[30];
-	char address[30];
-	int phone_number;
+    int id;
+    char name[30];
+    char address[30];
+    char phone_number[15];
 };
 
 struct order
 {
-	int id;
-	char product_id[30];
-	char product_name[30];
-	float price;
-	float final_sum;
-	char payment[100];
-	char status[30];
-	int user_id;
-	int cart_id;
+    int id;
+    char product_id[30];
+    char product_name[30];
+    float price;
+    float final_sum;
+    char payment[100];
+    char status[30];
+    int user_id;
+    int cart_id;
 };
 
-// Hàm xin chào
-void sayHello() {
-    printf("--------------------------------------\n");
-    printf("\tXin chao, chao mung den voi Shoppu.\n");
-}
 
-// Hàm hiển thị menu
-void menu() {
-    printf("--------------------------------------\n");
-    printf("1. Them san pham moi\n");
-    printf("2. Cap nhat thong tin san pham\n");
-    printf("3. Xoa san pham\n");
-    printf("4. Cap nhat gio hang\n");
-    printf("5. Xoa gio hang\n");
-    printf("6. Cap nhat hoa don\n");
-    printf("7. Xoa hoa don\n");
-    printf("0. Thoat\n");
-    printf("--------------------------------------\n");
-}
-
-
-//Nhập dữ liệu sản phẩm từ file
-void readProduct(struct product **products, int *productCount) {
-    char line[1000];
-    char *sp;
-    FILE *file;
-    file = fopen("product.csv", "r");
-    if (file == NULL) {
-        printf("Khong the mo file.\n");
-        return;
-    }
-    else {
-        while (fgets(line, 1000, file) != NULL) {
-            (*productCount)++;
-            *products = (struct product *)realloc(*products, *productCount * sizeof(struct product));
-            sp = strtok(line, ",");
-            (*products)[*productCount - 1].id = atoi(sp);
-            sp = strtok(NULL, ",");
-            strcpy((*products)[*productCount - 1].product_name, sp);
-            sp = strtok(NULL, ",");
-            (*products)[*productCount - 1].remain = atoi(sp);
-            sp = strtok(NULL, ",");
-            (*products)[*productCount - 1].price = atof(sp);
-        }
-        fclose(file);
-    }
-}
-
-//Hàm hiển thị sản phẩm
-void displayProduct(struct product *products, int productCount) {
-    printf("Danh sach san pham: \n");
-    for (int i = 0; i < productCount; i++) {
-        printf("ID: %d\n", products[i].id);
-        printf("Ten san pham: %s\n", products[i].product_name);
-        printf("So luong con lai: %d\n", products[i].remain);
-        printf("Gia: %.2f\n", products[i].price);
-        printf("--------------------------------------\n");
-    }
-}
 
 // Hàm thêm sản phẩm mới
 void addProduct(struct product **products, int *productCount, int id, char *name, int remain, float price) {
-    FILE *file;
-    file = fopen("product.csv", "a");
-    if (file == NULL) {
-        printf("Khong the mo file.\n");
-        return;
-    }
-    else {
-        fprintf(file, "%d,%s,%d,%.2f\n", id, name, remain, price);
-        fclose(file);
-        printf("Them thanh cong.\n");
-    }
+    *products = (struct product *)realloc(*products, (*productCount + 1) * sizeof(struct product));
+    (*products)[*productCount].id = id;
+    strcpy((*products)[*productCount].product_name, name);
+    (*products)[*productCount].remain = remain;
+    (*products)[*productCount].price = price;
+    (*productCount)++;
+    printf("Da them san pham moi thanh cong.\n");
 }
 
 // Hàm cập nhật thông tin sản phẩm
@@ -147,22 +86,6 @@ void deleteProduct(struct product **products, int *productCount, int id) {
     }
     if (!found) {
         printf("Khong tim thay hoa don co ID tuong ung.\n");
-    }
-}
-
-// Hàm hiển thị danh sách giỏ hàng
-void dissplayCart(struct cart *carts, int cartCount) {
-    printf("Danh sach gio hang: \n");
-    for (int i = 0; i < cartCount; i++) {
-        printf("ID: %d\n", carts[i].id);
-        printf("ID san pham: %s\n", carts[i].product_id);
-        printf("Ten san pham: %s\n", carts[i].pruct_name);
-        printf("Gia: %.2f\n", carts[i].price);
-        printf("Tong gia: %.2f\n", carts[i].sum);
-        printf("Ma giam gia: %s\n", carts[i].coupon);
-        printf("Tong gia sau khi giam: %.2f\n", carts[i].final_sum);
-        printf("ID nguoi dung: %d\n", carts[i].user_id);
-        printf("--------------------------------------\n");
     }
 }
 
@@ -243,8 +166,16 @@ void deleteOrder(struct order **orders, int *orderCount, int id) {
 
     int choice;
     do {
-        readProduct(&products, &productCount);
-        menu();
+        printf("\n====== MENU ======\n");
+        printf("1. Them san pham moi\n");
+        printf("2. Cap nhat thong tin san pham\n");
+        printf("3. Xoa san pham\n");
+        printf("4. Cap nhat gio hang\n");
+        printf("5. Xoa gio hang");
+        printf("6. Cap nhat hoa don\n");
+        printf("7. Xoa hoa don\n");
+        printf("0. Thoat\n");
+        printf("Nhap lua chon cua ban :");
         scanf("%d", &choice);
 
         switch (choice) {
@@ -266,7 +197,6 @@ void deleteOrder(struct order **orders, int *orderCount, int id) {
             case 2: {
                 int id, new_remain;
                 float new_price;
-                displayProduct(products, productCount);
                 printf("Nhap ID san pham can cap nhat : ");
                 scanf("%d", &id);
                 printf("Nhap so luong moi: ");
@@ -278,7 +208,6 @@ void deleteOrder(struct order **orders, int *orderCount, int id) {
             }
             case 3: {
                 int id;
-                displayProduct(products, productCount);
                 printf("Nhap ID san pham can xoa : ");
                 scanf("%d", &id);
                 deleteProduct(&products, &productCount, id);
@@ -287,7 +216,6 @@ void deleteOrder(struct order **orders, int *orderCount, int id) {
             case 4: {
                 int id;
                 float new_final_sum;
-                dissplayCart(carts, cartCount);
                 printf("Nhap ID gio hang can cap nhat : ");
                 scanf("%d", &id);
                 printf("Nhap tong gia moi : ");
@@ -322,7 +250,6 @@ void deleteOrder(struct order **orders, int *orderCount, int id) {
             case 0:
                 printf("Thoat chuong trinh. \n");
                 break;
-
             default:
                 printf("Khong ton tai.\n");
                 break;
