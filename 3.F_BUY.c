457802ecc6605692e0462845c9b9d67e783aa0a5
Kuzar365 <vothanhphuc365@gmail.com>
2024-04-26 10:56:23 +0700

    Change

--- 3.F_BUY.c
+++ 3.F_BUY.c
@@ -1,29 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+struct product
+{
+	int id;
+	char product_name[30];
+	int remain;
+	int price;
+};
+
+struct cart
+{
+	int id;
+	char product_id[30];
+	char product_name[30];
+	int price;
+	int sum;
+	int final_sum;
+	int user_id;
+};
+
+int lastAssignedID = 0;
 
 
 //import data from product.csv
-void readProduct(struct product *Arrproduct, int *number)
-{
-	FILE *file;
-	file = fopen("E:/Project C/product.csv", "r");
-	if (file == NULL)
-	{
-		printf("Can't open file\n");
-	}
-	else
-	{
-		while (!feof(file))
-		{
-			fscanf(file, "%d,%c,%d,%f\n", &Arrproduct[*number].id);
-			fscanf(file, "%c",Arrproduct[*number].product_name);
-			fscanf(file, "%d", &Arrproduct[*number].remain);
-			fscanf(file, "%f", &Arrproduct[*number].price);
-			(*number)++;
-		}
-	}
-	fclose(file);
-}
+void readProduct(struct product **products, int *productCount) {
+    char line[1000];
+    char *data;
+    FILE * productData = fopen("product.csv", "r");
+    if (productData == NULL) {
+        printf("Error opening file\n");
+        return;
+    }
+    else {
+        while (fgets(line, 100, productData) != NULL) {
+            (*productCount)++;
+            *products = (struct product *)realloc(*products, *productCount * sizeof(struct product));
 
+            data = strtok(line, ",");
+            (*products)[*productCount - 1].id = atoi(data);
+
+            data = strtok(NULL, ",");
+            strcpy((*products)[*productCount - 1].product_name, data);
+
+            data = strtok(NULL, ",");
+            (*products)[*productCount - 1].remain = atoi(data);
+
+            data = strtok(NULL, ",");
+            (*products)[*productCount - 1].price = atoi(data);
+        }
+        fclose(productData);
+    }
+}
 
 //show product/;llkkkkkkkkk jkllkl
 void showProduct(struct product *Arrproduct, int number)
